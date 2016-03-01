aff0533e7a08fcaf0904ac17b33ababb35a7b2f5
Giovanni Collazo <hello@gcollazo.com>
2016-03-01 18:51:22 -0400

    Only update if needed

--- update.js
+++ update.js
@@ -1,10 +1,21 @@
 /* eslint-disable camelcase */
 require('dotenv').load();
 
-const fetchAddons = require('./lib/ember-addons');
+const emberAddons = require('./lib/ember-addons');
 const createRssGenerator = require('./lib/rss');
 const createS3FileUploader = require('./lib/s3');
 const createDatabase = require('./lib/db');
+
+function createTimer() {
+  const startTime = new Date().getTime();
+
+  return function() {
+    const totalTime = (new Date().getTime() - startTime) / 1000;
+    console.log('--> Duration: ' + totalTime + 's');
+  };
+}
+
+const printTotalTime = createTimer();
 
 // Init
 const db = createDatabase({
@@ -26,47 +37,73 @@ const s3FileUploader = createS3FileUploader({
   bucket: process.env.AWS_BUCKET_NAME
 });
 
-const startTime = new Date().getTime();
+// Check if we need an update
+db.getLatestTotalMetric().then((lastCount) => {
+  lastCount = parseInt(lastCount, 10);
 
-
-// Update addons
-console.log('--> Fetching data from npm registry...');
-
-fetchAddons()
-  .then(function(addons) {
-    console.log('--> Done fetching data.');
-
-    console.log('--> Creating Feed...');
-    const rssFeed = rssGenerator(addons);
-
-    const uploadAddons = s3FileUploader({
-      data: JSON.stringify(addons),
-      fileName: process.env.ADDON_JSON_FILENAME,
-      contentType: 'application/json'
-    });
-
-    const uploadFeed = s3FileUploader({
-      data: rssFeed,
-      fileName: process.env.FEED_FILENAME,
-      contentType: 'application/rss+xml'
-    });
-
-    // Save files to S3
-    return uploadAddons()
-      .then(uploadFeed)
-      .then(function() { return addons; });
-  })
-  .then(function(addons) {
-    console.log('--> Done updating %s addons.', addons.length);
-    return db.updateTotalMetric(addons.length);
-  })
-  .catch(function(err) {
-    console.error('--> ERROR:', err);
-  })
-  .finally(function() {
-    db.close();
-
-    const MILLIS_SECOND = 1000;
-    const totalTime = (new Date().getTime() - startTime) / MILLIS_SECOND;
-    console.log('--> Duration: ' + totalTime + 's');
+  return emberAddons.getAll().then((allAddons) => {
+    if (lastCount !== allAddons.length) {
+      console.log(
+        '--> Needs update: YES --',
+        'lastCount:', lastCount,
+        'newCount:', allAddons.length
+      );
+      return allAddons;
+    } else {
+      console.log(
+        '--> Needs update: NO --',
+        'lastCount:', lastCount,
+        'newCount:', allAddons.length
+      );
+      printTotalTime();
+      process.exit(0);
+    }
   });
+})
+.then(function(addons) {
+  // Update addons
+  console.log('--> Fetching data from npm registry...');
+  return emberAddons.getDetails(addons);
+})
+.then(function(addons) {
+  console.log('--> Done fetching data.');
+
+  console.log('--> Creating Feed...');
+  const rssFeed = rssGenerator(addons);
+
+  const uploadAddons = s3FileUploader({
+    data: JSON.stringify(addons),
+    fileName: process.env.ADDON_JSON_FILENAME,
+    contentType: 'application/json'
+  });
+
+  const uploadFeed = s3FileUploader({
+    data: rssFeed,
+    fileName: process.env.FEED_FILENAME,
+    contentType: 'application/rss+xml'
+  });
+
+  const lastUpdated = s3FileUploader({
+    data: JSON.stringify({ date: new Date() }),
+    fileName: process.env.ADDON_LAST_UPDATED_FILENAME,
+    contentType: 'application/json'
+  });
+
+  // Save files to S3
+  return uploadAddons()
+    .then(uploadFeed)
+    .then(lastUpdated)
+    .then(function() { return addons; });
+})
+.then(function(addons) {
+  console.log('--> Done updating %s addons.', addons.length);
+  return db.updateTotalMetric(addons.length);
+})
+.catch(function(err) {
+  console.error('--> ERROR:', err);
+})
+.finally(function() {
+  db.close();
+  printTotalTime();
+  process.exit(0);
+});
