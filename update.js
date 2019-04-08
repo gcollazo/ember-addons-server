1a5b217c862c8db3d7b29d79fde3a3f9ee58109b
Giovanni Collazo <hello@gcollazo.com>
2019-04-07 22:29:43 -0400

    Allow opt out of upload compression

--- update.js
+++ update.js
@@ -84,7 +84,8 @@ async function run() {
     let uploadStats = s3FileUploader({
       data: csv(stats),
       fileName: process.env.STATS_FILENAME,
-      contentType: 'text/csv'
+      contentType: 'text/csv',
+      compressData: false
     });
     await uploadStats();
 
