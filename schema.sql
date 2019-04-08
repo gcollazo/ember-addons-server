82151fda2644339a11831a4c7f0556f56c52832c
Giovanni Collazo <hello@gcollazo.com>
2019-04-07 20:43:42 -0400

    Update schema

--- schema.sql
+++ schema.sql
@@ -16,30 +16,30 @@
 */
 
 -- ----------------------------
---  Sequence structure for addos_id_seq
+--  Sequence structure for addons_id_seq
 -- ----------------------------
-DROP SEQUENCE IF EXISTS "public"."addos_id_seq";
-CREATE SEQUENCE "public"."addos_id_seq" INCREMENT 1 START 1854 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
-ALTER TABLE "public"."addos_id_seq" OWNER TO "gcollazo";
+DROP SEQUENCE IF EXISTS "public"."addons_id_seq";
+CREATE SEQUENCE "public"."addons_id_seq" INCREMENT 1 START 1854 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
+ALTER TABLE "public"."addons_id_seq" OWNER TO "emberaddonsuser";
 
 -- ----------------------------
 --  Sequence structure for stats_id_seq
 -- ----------------------------
 DROP SEQUENCE IF EXISTS "public"."stats_id_seq";
 CREATE SEQUENCE "public"."stats_id_seq" INCREMENT 1 START 2 MAXVALUE 9223372036854775807 MINVALUE 1 CACHE 1;
-ALTER TABLE "public"."stats_id_seq" OWNER TO "gcollazo";
+ALTER TABLE "public"."stats_id_seq" OWNER TO "emberaddonsuser";
 
 -- ----------------------------
 --  Table structure for addons
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."addons";
 CREATE TABLE "public"."addons" (
-	"id" int4 NOT NULL DEFAULT nextval('addos_id_seq'::regclass),
+	"id" int4 NOT NULL DEFAULT nextval('addons_id_seq'::regclass),
 	"name" varchar COLLATE "default",
 	"doc" json
 )
 WITH (OIDS=FALSE);
-ALTER TABLE "public"."addons" OWNER TO "gcollazo";
+ALTER TABLE "public"."addons" OWNER TO "emberaddonsuser";
 
 -- ----------------------------
 --  Table structure for metrics
@@ -52,13 +52,13 @@ CREATE TABLE "public"."metrics" (
 	"value" numeric NOT NULL
 )
 WITH (OIDS=FALSE);
-ALTER TABLE "public"."metrics" OWNER TO "gcollazo";
+ALTER TABLE "public"."metrics" OWNER TO "emberaddonsuser";
 
 
 -- ----------------------------
 --  Alter sequences owned by
 -- ----------------------------
-ALTER SEQUENCE "public"."addos_id_seq" RESTART 1855 OWNED BY "addons"."id";
+ALTER SEQUENCE "public"."addons_id_seq" RESTART 1855 OWNED BY "addons"."id";
 ALTER SEQUENCE "public"."stats_id_seq" RESTART 3 OWNED BY "metrics"."id";
 -- ----------------------------
 --  Primary key structure for table addons
